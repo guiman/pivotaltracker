46206247a0a07c94a295e2d13846f6e667af72fa
Alvaro F. Lara <alvaro.lara@alliants.com>
2015-02-26 09:28:03 +0000

    Make VCR ignore codeclimate calls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,6 +20,7 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = File.join(__dir__, 'fixtures', 'vcr_cassettes')
   config.hook_into :webmock
+  config.ignore_hosts 'codeclimate.com'
 end
 
 module PivotalTracker
